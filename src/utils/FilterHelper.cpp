0ae42ea82e66d9c74a28ba73ac8863d1b393d026
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-18 04:29:39 +0200

    Create FilterHelper.cpp

---
+++ src/utils/FilterHelper.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+#include <json/json.h>
+
+/**
+ * @brief Main function that serves as the entry point of the program.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv An array of C-strings containing the command-line arguments.
+ * @return The exit status of the program.
+ */
+int main(int argc, char** argv) {
+    return 0;
+}
+
