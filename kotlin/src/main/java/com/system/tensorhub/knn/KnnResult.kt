0d4170dd00ac44090249d9a232eeaf425c50a050
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-18 06:49:25 +0200

    Update KnnResult.kt

--- kotlin/src/main/java/com/system/tensorhub/knn/KnnResult.kt
+++ kotlin/src/main/java/com/system/tensorhub/knn/KnnResult.kt
@@ -10,44 +10,35 @@ package com.system.tensorhub.knn
 class KnnResult(private val keys: Array<String>, private val scores: FloatArray, private val k: Int) {
 
     init {
-        val slen = scores.size
-        val ilen = keys.size
-        if (slen != ilen) {
-            throw IllegalArgumentException("scores and indexes have different lengths (scores: $slen, indexes: $ilen)")
-        }
-        if (slen % k != 0) {
-            throw IllegalArgumentException("k: $k must divide the length of data: $slen")
-        }
+        require(scores.size == keys.size) { "Scores and keys have different lengths (scores: ${scores.size}, keys: ${keys.size})" }
+        require(scores.size % k == 0) { "k: $k must divide the length of data: ${scores.size}" }
     }
 
     /**
      * Returns the batch size of the KNN result.
-     *
-     * @return The batch size.
      */
-    fun getBatchSize(): Int {
-        return scores.size / k
-    }
+    val batchSize: Int
+        get() = scores.size / k
 
     /**
      * Returns the key at the specified row and index.
      *
-     * @param row The row index.
-     * @param i The index.
+     * @param rowIndex The row index.
+     * @param index The index.
      * @return The key.
      */
-    fun getKeyAt(row: Int, i: Int): String {
-        return keys[row * k + i]
+    fun getKeyAt(rowIndex: Int, index: Int): String {
+        return keys[rowIndex * k + index]
     }
 
     /**
      * Returns the score at the specified row and index.
      *
-     * @param row The row index.
-     * @param i The index.
+     * @param rowIndex The row index.
+     * @param index The index.
      * @return The score.
      */
-    fun getScoreAt(row: Int, i: Int): Float {
-        return scores[row * k + i]
+    fun getScoreAt(rowIndex: Int, index: Int): Float {
+        return scores[rowIndex * k + index]
     }
 }
