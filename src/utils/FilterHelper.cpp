bbe7603a01567cf52e8fe734f332dc25d7a40d31
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-18 21:07:20 +0200

    Update FilterHelper.cpp

--- src/utils/FilterHelper.cpp
+++ src/utils/FilterHelper.cpp
@@ -3,8 +3,34 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <charconv>
+#include <filesystem>
 
 #include <json/json.h>
+
+namespace fs = std::filesystem;
+
+/**
+ * @brief Read lines from a file and return them as a vector of strings.
+ *
+ * @param filePath The path to the file.
+ * @return A vector of strings containing the lines from the file.
+ * @throws std::runtime_error If the file cannot be opened.
+ */
+[[nodiscard]] std::vector<std::string> readLinesFromFile(const fs::path& filePath) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + filePath.string());
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
 
 /**
  * @brief Main function that serves as the entry point of the program.
@@ -14,6 +40,22 @@
  * @return The exit status of the program.
  */
 int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
+
+    fs::path filePath(argv[1]);
+
+    try {
+        auto lines = readLinesFromFile(filePath);
+        for (const auto& line : lines) {
+            // Process each line
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return 1;
+    }
+
     return 0;
 }
-
