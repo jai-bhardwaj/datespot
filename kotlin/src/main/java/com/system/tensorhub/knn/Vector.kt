76c28ad498bec91b261db93bfa15068d4994efe1
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-18 06:49:46 +0200

    Update Vector.kt

--- kotlin/src/main/java/com/system/tensorhub/knn/Vector.kt
+++ kotlin/src/main/java/com/system/tensorhub/knn/Vector.kt
@@ -1,22 +1,55 @@
-package com.system.tensorhub.knn;
+package com.system.tensorhub.knn
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.Objects
 
 /**
  * Represents a vector.
  */
-class Vector private constructor() {
-    var index: Int = 0
-        private set
-    var coordinates: List<Float>? = null
-        private set
+data class Vector(
+    val index: Int,
+    val coordinates: List<Float>
+) {
+    init {
+        require(index >= 0) { "Index must be non-negative" }
+    }
 
     /**
-     * Creates a new instance of the Vector.Builder.
+     * Computes the hash code of the vector.
      *
-     * @return The Vector.Builder instance.
+     * @return The computed hash code.
+     */
+    override fun hashCode(): Int {
+        return Objects.hash(index, coordinates)
+    }
+
+    /**
+     * Checks if this Vector is equal to the specified object.
+     *
+     * @param other The object to compare.
+     * @return True if the objects are equal, false otherwise.
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (other !is Vector) {
+            return false
+        }
+
+        return index == other.index && coordinates == other.coordinates
+    }
+
+    /**
+     * Returns a string representation of the Vector.
+     *
+     * @return The string representation of the Vector.
+     */
+    override fun toString(): String {
+        return "Vector(index=$index, coordinates=$coordinates)"
+    }
+
+    /**
+     * Builder class for constructing Vector instances.
      */
     companion object {
         fun builder(): Builder {
@@ -29,7 +62,7 @@ class Vector private constructor() {
      */
     class Builder {
         private var index: Int = 0
-        private var coordinates: List<Float>? = null
+        private var coordinates: List<Float> = emptyList()
 
         /**
          * Sets the index of the vector.
@@ -49,13 +82,8 @@ class Vector private constructor() {
          * @return The Builder instance.
          */
         fun withCoordinates(coordinates: List<Float>): Builder {
-            this.coordinates = coordinates
+            this.coordinates = coordinates.toList()
             return this
-        }
-
-        private fun populate(instance: Vector) {
-            instance.index = this.index
-            instance.coordinates = this.coordinates
         }
 
         /**
@@ -64,115 +92,7 @@ class Vector private constructor() {
          * @return The newly created Vector instance.
          */
         fun build(): Vector {
-            val instance = Vector()
-
-            populate(instance)
-
-            return instance
+            return Vector(index, coordinates)
         }
     }
-
-    ;
-
-    private var index: Int = 0
-    private var coordinates: List<Float>? = null
-    
-    /**
-     * Gets the index of the vector.
-     *
-     * @return The index value.
-     */
-    fun getIndex(): Int {
-        return index
-    }
-    
-    /**
-     * Sets the index of the vector.
-     *
-     * @param index The index value.
-     */
-    fun setIndex(index: Int) {
-        this.index = index
-    }
-    
-    /**
-     * Gets the coordinates of the vector.
-     *
-     * @return The list of coordinates.
-     */
-    val coordinates: List<Float>
-        get() = this.coordinates
-    
-    /**
-     * Sets the coordinates of the vector.
-     *
-     * @param coordinates The list of coordinates.
-     */
-    fun setCoordinates(coordinates: List<Float>) {
-        this.coordinates = coordinates
-    }
-    
-    private val classNameHashCode = internalHashCodeCompute("com.system.tensorhub.knn.Vector")
-    
-    /**
-     * Computes the hash code of the vector.
-     *
-     * @return The computed hash code.
-     */
-    override fun hashCode(): Int {
-        return internalHashCodeCompute(
-            classNameHashCode,
-            getIndex(),
-            getCoordinates()
-        )
-    }
-
-    /**
-     * Computes the hash code based on the provided objects.
-     *
-     * @param objects The objects used to compute the hash code.
-     * @return The computed hash code.
-     */
-    private fun internalHashCodeCompute(vararg objects: Any): Int {
-        return objects.contentHashCode()
-    }
-
-    /**
-     * Checks if this Vector is equal to the specified object.
-     *
-     * @param other The object to compare.
-     * @return True if the objects are equal, false otherwise.
-     */
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-        if (other !is Vector) {
-            return false
-        }
-
-        val that = other as Vector
-
-        return getIndex() == that.getIndex() && getCoordinates() == that.getCoordinates()
-    }
-
-    /**
-     * Returns a string representation of the Vector.
-     *
-     * @return The string representation of the Vector.
-     */
-    override fun toString(): String {
-        val ret = StringBuilder()
-        ret.append("Vector(")
-
-        ret.append("index=")
-        ret.append(index.toString())
-        ret.append(", ")
-
-        ret.append("coordinates=")
-        ret.append(coordinates.toString())
-        ret.append(")")
-
-        return ret.toString()
-    }    
 }
