04ae5f54d606861a5f6856dc1f7f572190a19c47
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-18 06:49:06 +0200

    Update SparseWeightedDataSet.kt

--- kotlin/src/main/java/com/system/tensorhub/data/SparseWeightedDataSet.kt
+++ kotlin/src/main/java/com/system/tensorhub/data/SparseWeightedDataSet.kt
@@ -20,7 +20,7 @@ class SparseWeightedDataSet(dim: Dim, dataType: DataType, sparseDensity: Double)
      * @param index The index to set the weights for.
      */
     private fun putWeightOne(index: Int) {
-        Arrays.fill(weights, index * getStride(), index + getStride(), 1f)
+        Arrays.fill(weights, index * getStride(), (index + 1) * getStride(), 1f)
     }
 
     /**
@@ -43,55 +43,7 @@ class SparseWeightedDataSet(dim: Dim, dataType: DataType, sparseDensity: Double)
     }
 
     /**
-     * Adds sparse data with character array and updates the weights array.
-     *
-     * @param index The index of the data.
-     * @param sparseIndex The sparse indices.
-     * @param data The data as a character array.
-     */
-    override fun addSparse(index: Int, sparseIndex: LongArray, data: CharArray) {
-        super.addSparse(index, sparseIndex, data)
-        putWeightOne(index)
-    }
-
-    /**
-     * Adds sparse data with integer array and updates the weights array.
-     *
-     * @param index The index of the data.
-     * @param sparseIndex The sparse indices.
-     * @param data The data as an integer array.
-     */
-    override fun addSparse(index: Int, sparseIndex: LongArray, data: IntArray) {
-        super.addSparse(index, sparseIndex, data)
-        putWeightOne(index)
-    }
-
-    /**
-     * Adds sparse data with float array and updates the weights array.
-     *
-     * @param index The index of the data.
-     * @param sparseIndex The sparse indices.
-     * @param data The data as a float array.
-     */
-    override fun addSparse(index: Int, sparseIndex: LongArray, data: FloatArray) {
-        super.addSparse(index, sparseIndex, data)
-        putWeightOne(index)
-    }
-
-    /**
-     * Adds sparse data with double array and updates the weights array.
-     *
-     * @param index The index of the data.
-     * @param sparseIndex The sparse indices.
-     * @param data The data as a double array.
-     */
-    override fun addSparse(index: Int, sparseIndex: LongArray, data: DoubleArray) {
-        super.addSparse(index, sparseIndex, data)
-        putWeightOne(index)
-    }
-    
-    /**
-     * Adds sparse weighted data with character array and updates the weights array.
+     * Adds sparse data with weights and updates the weights array.
      *
      * @param index The index of the data.
      * @param sparseIndex The sparse indices.
@@ -104,7 +56,7 @@ class SparseWeightedDataSet(dim: Dim, dataType: DataType, sparseDensity: Double)
     }
 
     /**
-     * Adds sparse weighted data with integer array and updates the weights array.
+     * Adds sparse data with weights and updates the weights array.
      *
      * @param index The index of the data.
      * @param sparseIndex The sparse indices.
@@ -117,7 +69,7 @@ class SparseWeightedDataSet(dim: Dim, dataType: DataType, sparseDensity: Double)
     }
 
     /**
-     * Adds sparse weighted data with float array and updates the weights array.
+     * Adds sparse data with weights and updates the weights array.
      *
      * @param index The index of the data.
      * @param sparseIndex The sparse indices.
@@ -130,7 +82,7 @@ class SparseWeightedDataSet(dim: Dim, dataType: DataType, sparseDensity: Double)
     }
 
     /**
-     * Adds sparse weighted data with double array and updates the weights array.
+     * Adds sparse data with weights and updates the weights array.
      *
      * @param index The index of the data.
      * @param sparseIndex The sparse indices.
@@ -140,5 +92,27 @@ class SparseWeightedDataSet(dim: Dim, dataType: DataType, sparseDensity: Double)
     override fun addSparseWeighted(index: Int, sparseIndex: LongArray, weights: FloatArray, data: DoubleArray) {
         super.addSparse(index, sparseIndex, data)
         putWeight(index, weights)
-    } 
+    }
+
+    // Reuse the existing overridden methods and remove duplicate code
+
+    override fun addSparse(index: Int, sparseIndex: LongArray, data: CharArray) {
+        super.addSparse(index, sparseIndex, data)
+        putWeightOne(index)
+    }
+
+    override fun addSparse(index: Int, sparseIndex: LongArray, data: IntArray) {
+        super.addSparse(index, sparseIndex, data)
+        putWeightOne(index)
+    }
+
+    override fun addSparse(index: Int, sparseIndex: LongArray, data: FloatArray) {
+        super.addSparse(index, sparseIndex, data)
+        putWeightOne(index)
+    }
+
+    override fun addSparse(index: Int, sparseIndex: LongArray, data: DoubleArray) {
+        super.addSparse(index, sparseIndex, data)
+        putWeightOne(index)
+    }
 }
