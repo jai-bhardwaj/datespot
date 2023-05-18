66eeeecd687cabb69c2e3715f30eb1fe0873ac2d
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-18 04:31:14 +0200

    Update cdl.h

--- src/utils/cdl.h
+++ src/utils/cdl.h
@@ -1,11 +1,11 @@
-#ifndef __CDL_H__
-#define __CDL_H__
+#pragma once
 
+#include <string>
 #include "GpuTypes.h"
 #include "Types.h"
 
 /**
- * @brief The CDL struct represents a configuration for a CDL (Convolutional Deep Learning) model.
+ * @brief Struct representing CDL (Compressed Deep Learning) configuration.
  */
 struct CDL
 {
@@ -15,29 +15,27 @@ struct CDL
     CDL();
 
     /**
-     * @brief Load the CDL configuration from a JSON file.
-     * @param fname The filename of the JSON file.
-     * @return The status of the loading operation.
+     * @brief Load JSON configuration from file.
+     * @param fname The name of the JSON file to load.
+     * @return The status of the load operation.
      */
-    int Load_JSON(const string& fname);
+    int Load_JSON(const std::string& fname);
 
-    std::string     _networkFileName;       // The filename of the network file.
-    int             _randomSeed;            // The random seed value.
-    Mode            _mode;                  // The mode of operation.
-    std::string     _dataFileName;          // The filename of the data file.
+    std::string     _networkFileName;       /**< The file name of the network. */
+    int             _randomSeed;            /**< The random seed value. */
+    Mode            _mode;                  /**< The mode of operation. */
+    std::string     _dataFileName;          /**< The file name of the data. */
 
-    int             _epochs;                // The number of training epochs.
-    int             _batch;                 // The batch size.
-    float           _alpha;                 // The learning rate.
-    float           _lambda;                // The regularization parameter.
-    float           _mu;                    // The momentum parameter.
-    int             _alphaInterval;         // The interval at which to update the learning rate.
-    float           _alphaMultiplier;       // The multiplier to adjust the learning rate.
-    TrainingMode    _optimizer;             // The optimizer to use for training.
-    std::string     _checkpointFileName;    // The filename to save checkpoints.
-    int             _checkpointInterval;    // The interval at which to save checkpoints.
-    bool            _shuffleIndexes;        // Whether to shuffle the indexes during training.
-    std::string     _resultsFileName;       // The filename to save training results.
+    int             _epochs;                /**< The number of epochs. */
+    int             _batch;                 /**< The batch size. */
+    float           _alpha;                 /**< The learning rate. */
+    float           _lambda;                /**< The regularization parameter. */
+    float           _mu;                    /**< The momentum parameter. */
+    int             _alphaInterval;         /**< The interval for changing the learning rate. */
+    float           _alphaMultiplier;       /**< The multiplier for changing the learning rate. */
+    TrainingMode    _optimizer;             /**< The optimization algorithm. */
+    std::string     _checkpointFileName;    /**< The file name for checkpointing. */
+    int             _checkpointInterval;    /**< The interval for checkpointing. */
+    bool            _shuffleIndexes;        /**< Flag indicating whether to shuffle indexes. */
+    std::string     _resultsFileName;       /**< The file name for storing results. */
 };
-
-#endif
