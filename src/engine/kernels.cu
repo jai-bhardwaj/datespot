52a6a509d9d8bd38279af17b40fa56c92d944d4d
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-19 18:42:07 +0200

    Update kernels.cu

--- src/engine/kernels.cu
+++ src/engine/kernels.cu
@@ -112,3 +112,319 @@ void kClearQuadSourceUnit(Float* pUnit, Float* pBias1, Float* pBias2, Float* pBi
     kClearQuadSourceUnit_kernel<<<blocks, threadsPerBlock>>>(pUnit, pBias1, pBias2, pBias3, pBias4, stride, size);
     LAUNCHERROR("kClearQuadSourceUnit_kernel");
 }
+__global__ void kLoadSparseInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = pos + position;
+        pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[pos1] : pos1;
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = (pDataWeight != NULL) ? pDataWeight[pos1] : (Float)1.0;
+        uint64_t offset = pos * stride;
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            uint64_t pos2 = offset + pSparseIndex[i];
+            pUnit[pos2] = w;
+        }
+    }
+}
+
+void kLoadSparseInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadSparseInputUnit failed");
+    kLoadSparseInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight);
+    LAUNCHERROR("kLoadSparseInputUnit_kernel");
+}
+__global__ void kLoadIndexedSparseInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = pos + position;
+        pos1 = pIndex[cData._bShuffleIndices ? cData._pShuffleIndex[pos1] : pos1];
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = (pDataWeight != NULL) ? pDataWeight[pos1] : (Float)1.0;
+        uint64_t offset = pos * stride;
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            uint64_t pos2 = offset + pSparseIndex[i];
+            pUnit[pos2] = w;
+        }
+    }
+}
+
+void kLoadIndexedSparseInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadIndexedSparseInputUnit failed");
+    kLoadIndexedSparseInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pIndex, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight);
+    LAUNCHERROR("kLoadIndexedSparseInputUnit_kernel");
+}
+template<typename T>
+__global__ void kLoadSparseAnalogInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = pos + position;
+        pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[pos1] : pos1;
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = (pDataWeight != nullptr) ? pDataWeight[pos1] : static_cast<Float>(1.0);
+        uint64_t offset = static_cast<uint64_t>(pos) * static_cast<uint64_t>(stride);
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            uint64_t pos2 = offset + pSparseIndex[i];
+            T data = pSparseData[i];
+            pUnit[pos2] = w * static_cast<Float>(data);
+        }
+    }
+}
+
+template<typename T>
+void kLoadSparseAnalogInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadSparseAnalogInputUnit failed");
+    kLoadSparseAnalogInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight, pSparseData);
+    LAUNCHERROR("kLoadSparseAnalogInputUnit_kernel");
+}
+template<typename T>
+__global__ void kLoadIndexedSparseAnalogInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = pos + position;
+        pos1 = pIndex[cData._bShuffleIndices ? cData._pShuffleIndex[pos1] : pos1];
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = (pDataWeight != nullptr) ? pDataWeight[pos1] : static_cast<Float>(1.0);
+        uint64_t offset = static_cast<uint64_t>(pos) * static_cast<uint64_t>(stride);
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            uint64_t pos2 = offset + pSparseIndex[i];
+            T data = pSparseData[i];
+            pUnit[pos2] = w * static_cast<Float>(data);
+        }
+    }
+}
+
+template<typename T>
+void kLoadIndexedSparseAnalogInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadIndexedSparseAnalogInputUnit failed");
+    kLoadIndexedSparseAnalogInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pIndex, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight, pSparseData);
+    LAUNCHERROR("kLoadIndexedSparseAnalogInputUnit_kernel");
+}
+__global__ void kLoadSparseDenoisedInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, Float* pRandom)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[pos + position] : pos + position;
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = cData._denoising_q * ((pDataWeight != nullptr) ? pDataWeight[pos1] : static_cast<Float>(1.0));
+        uint64_t offset = static_cast<uint64_t>(pos) * static_cast<uint64_t>(stride);
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            Float value = pRandom[i];
+            uint64_t pos2 = offset + pSparseIndex[i];
+            if (value >= cData._denoising_p)
+                pUnit[pos2] = w;
+        }
+    }
+}
+
+void kLoadSparseDenoisedInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, Float* pRandom)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadSparseDenoisedInputUnit failed");
+    kLoadSparseDenoisedInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight, pRandom);
+    LAUNCHERROR("kLoadSparseDenoisedInputUnit_kernel");
+}
+__global__ void kLoadIndexedSparseDenoisedInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, Float* pRandom)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = pIndex[cData._bShuffleIndices ? cData._pShuffleIndex[pos + position] : pos + position];
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = cData._denoising_q * ((pDataWeight != nullptr) ? pDataWeight[pos1] : static_cast<Float>(1.0));
+        uint64_t offset = static_cast<uint64_t>(pos) * static_cast<uint64_t>(stride);
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            Float value = pRandom[i];
+            uint64_t pos2 = offset + pSparseIndex[i];
+            if (value >= cData._denoising_p)
+                pUnit[pos2] = w;
+        }
+    }
+}
+
+void kLoadIndexedSparseDenoisedInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, Float* pRandom)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadIndexedSparseDenoisedInputUnit failed");
+    kLoadIndexedSparseDenoisedInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pIndex, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight, pRandom);
+    LAUNCHERROR("kLoadIndexedSparseDenoisedInputUnit_kernel");
+}
+template<typename T>
+__global__ void kLoadSparseAnalogDenoisedInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData, Float* pRandom)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[pos + position] : pos + position;
+        uint64_t start = pSparseStart[pos1];
+        uint64_t end = pSparseEnd[pos1];
+        Float w = cData._denoising_q * ((pDataWeight != nullptr) ? pDataWeight[pos1] : static_cast<Float>(1.0));
+        uint64_t offset = static_cast<uint64_t>(pos) * static_cast<uint64_t>(stride);
+        for (uint64_t i = start + threadIdx.x; i < end; i += blockDim.x)
+        {
+            Float value = pRandom[i];
+            uint64_t pos2 = offset + pSparseIndex[i];
+            T data = pSparseData[i];
+            if (value >= cData._denoising_p)
+                pUnit[pos2] = w * data;
+        }
+    }
+}
+
+template<typename T>
+void kLoadSparseAnalogDenoisedInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData, Float* pRandom)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadSparseAnalogDenoisedInputUnit failed");
+    kLoadSparseAnalogDenoisedInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight, pSparseData, pRandom);
+    LAUNCHERROR("kLoadSparseAnalogDenoisedInputUnit_kernel");
+}
+template<typename T>
+__global__ void kLoadIndexedSparseAnalogDenoisedInputUnit_kernel(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData, Float* pRandom)
+{
+    uint32_t pos = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pos < batch)
+    {
+        uint32_t pos1 = pIndex[pos + position];
+        pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[pos1] : pos1;
+        uint64_t start = pSparseStart[pos1] + threadIdx.x;
+        uint64_t end = pSparseEnd[pos1];
+        Float w = cData._denoising_q * ((pDataWeight != nullptr) ? pDataWeight[pos1] : static_cast<Float>(1.0));
+        uint64_t offset = static_cast<uint64_t>(pos) * static_cast<uint64_t>(stride);
+        for (uint64_t i = start; i < end; i += blockDim.x)
+        {
+            Float value = pRandom[i];
+            uint64_t pos2 = offset + pSparseIndex[i];
+            T data = pSparseData[i];
+            if (value >= cData._denoising_p)
+                pUnit[pos2] = w * data;
+        }
+    }
+}
+
+template<typename T>
+void kLoadIndexedSparseAnalogDenoisedInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, uint32_t* pIndex, uint64_t* pSparseStart, uint64_t* pSparseEnd, uint32_t* pSparseIndex, Float* pDataWeight, T* pSparseData, Float* pRandom)
+{
+    uint32_t last = position + batch;
+    uint32_t count = last - position;
+    uint32_t threadsPerBlock = getGpu()._threadsPerBlock;
+    uint32_t blocks = (count + threadsPerBlock - 1) / threadsPerBlock;
+
+    cudaError_t status = cudaMemset(pUnit, 0, static_cast<uint64_t>(batch) * static_cast<uint64_t>(stride) * sizeof(Float));
+    RTERROR(status, "kLoadIndexedSparseAnalogDenoisedInputUnit failed");
+    kLoadIndexedSparseAnalogDenoisedInputUnit_kernel<<<blocks, threadsPerBlock>>>(position, batch, stride, pUnit, pIndex, pSparseStart, pSparseEnd, pSparseIndex, pDataWeight, pSparseData, pRandom);
+    LAUNCHERROR("kLoadIndexedSparseAnalogDenoisedInputUnit_kernel");
+}
+template<typename T>
+__global__ void kLoadInputUnit_kernel(uint32_t position, uint32_t stride, Float* pUnit, T* pData)
+{
+    uint32_t blockIdx_x = blockIdx.x;
+    uint64_t pos = (blockIdx_x * blockDim.x) + threadIdx.x;
+    if (pos < stride)
+    {
+        uint32_t pos1 = blockIdx_x + position;
+        pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[pos1] : pos1;
+        uint64_t soffset = static_cast<uint64_t>(pos1) * static_cast<uint64_t>(stride) + pos;
+        uint64_t doffset = static_cast<uint64_t>(blockIdx_x) * static_cast<uint64_t>(stride) + pos;
+        pUnit[doffset] = static_cast<Float>(pData[soffset]);
+    }
+}
+__global__ void kLoadNormalizedInputUnit_kernel(int position, int stride, Float* pUnit, unsigned char* pData)
+{
+    int tid = (blockIdx.y * blockDim.x) + threadIdx.x;
+    if (tid < stride)
+    {
+        int pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[blockIdx.x + position] : (blockIdx.x + position);
+        int offset = blockIdx.x * stride + tid;
+        pUnit[offset] = static_cast<Float>(pData[pos1 * stride + tid]) * cData._inv256 - cData._half;
+    }
+}
+__global__ void kLoadNormalizedInputUnit_kernel(uint32_t position, uint32_t stride, Float* pUnit, unsigned char* pData)
+{
+    uint64_t pos = (blockIdx.y * blockDim.x) + threadIdx.x;
+    if (pos < stride)
+    {
+        uint32_t pos1 = cData._bShuffleIndices ? cData._pShuffleIndex[blockIdx.x + position] : (blockIdx.x + position);
+        uint64_t soffset = pos1 * stride + pos;
+        uint64_t doffset = blockIdx.x * stride + pos;
+        pUnit[doffset] = static_cast<Float>(pData[soffset]) * cData._inv128;
+    }
+}
+
+template<typename T>
+void kLoadInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, T* pData)
+{
+    dim3 grid(batch, (stride + getGpu()._threadsPerBlock - 1) / getGpu()._threadsPerBlock);
+    kLoadInputUnit_kernel<<<grid, getGpu()._threadsPerBlock>>>(position, stride, pUnit, pData);
+    LAUNCHERROR("kLoadInputUnit_kernel");
+}
+
+template<>
+void kLoadInputUnit(uint32_t position, uint32_t batch, uint32_t stride, Float* pUnit, unsigned char* pData)
+{
+    dim3 grid(batch, (stride + getGpu()._threadsPerBlock - 1) / getGpu()._threadsPerBlock);
+    kLoadNormalizedInputUnit_kernel<<<grid, getGpu()._threadsPerBlock>>>(position, stride, pUnit, pData);
+    LAUNCHERROR("kLoadNormalizedInputUnit_kernel");
+}
