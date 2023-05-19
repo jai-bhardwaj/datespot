e2b60c642fb549fb722ad07e7ebd919987cc980b
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-19 06:55:32 +0200

    Update bitonic.h

--- src/engine/bitonic.h
+++ src/engine/bitonic.h
@@ -1,6 +1,11 @@
 #ifndef BITONIC_H
 #define BITONIC_H
 
+/**
+ * @brief Perform bitonic warp exchange for 64-bit values based on a given mask.
+ *
+ * @param mask The mask used for bitonic warp exchange.
+ */
 #define BITONICWARPEXCHANGE_64(mask) \
     for (int i = 0; i < 2; i++) { \
         int idx1 = i; \
@@ -16,24 +21,30 @@
         v[idx1] = flag ? value1 : value2; \
     }
 
+/**
+ * @brief Perform bitonic sort for 32-bit values using 64-bit warp exchange operations.
+ */
 #define BITONICSORT32_64() \
-    BITONICWARPEXCHANGE_64(1) \
-    BITONICWARPEXCHANGE_64(3) \
-    BITONICWARPEXCHANGE_64(1) \
-    BITONICWARPEXCHANGE_64(7) \
-    BITONICWARPEXCHANGE_64(2) \
-    BITONICWARPEXCHANGE_64(1) \
-    BITONICWARPEXCHANGE_64(15) \
-    BITONICWARPEXCHANGE_64(4) \
-    BITONICWARPEXCHANGE_64(2) \
-    BITONICWARPEXCHANGE_64(1) \
-    BITONICWARPEXCHANGE_64(31) \
-    BITONICWARPEXCHANGE_64(8) \
-    BITONICWARPEXCHANGE_64(4) \
-    BITONICWARPEXCHANGE_64(2) \
-    BITONICWARPEXCHANGE_64(1) 
+    BITONICWARPEXCHANGE_64(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_64(3) /**< Perform bitonic warp exchange with mask 3 */ \
+    BITONICWARPEXCHANGE_64(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_64(7) /**< Perform bitonic warp exchange with mask 7 */ \
+    BITONICWARPEXCHANGE_64(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_64(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_64(15) /**< Perform bitonic warp exchange with mask 15 */ \
+    BITONICWARPEXCHANGE_64(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_64(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_64(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_64(31) /**< Perform bitonic warp exchange with mask 31 */ \
+    BITONICWARPEXCHANGE_64(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_64(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_64(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_64(1) /**< Perform bitonic warp exchange with mask 1 */
 
 
+/**
+ * @brief Perform bitonic merge for 64-bit values using 64-bit warp exchange operations.
+ */
 #define BITONICMERGE64_64() \
     for (int i = 0; i < 2; i++) { \
         int idx1 = i; \
@@ -57,15 +68,23 @@
         v[idx1 + 2] = flag ? value1 : value2; \
     }
 
+/**
+ * @brief Perform bitonic sort for 64-bit values using 64-bit warp exchange and merge operations.
+ */
 #define BITONICSORT64_64() \
-    BITONICSORT32_64() \
-    BITONICMERGE64_64() \
-    BITONICWARPEXCHANGE_64(16) \
-    BITONICWARPEXCHANGE_64(8) \
-    BITONICWARPEXCHANGE_64(4) \
-    BITONICWARPEXCHANGE_64(2) \
-    BITONICWARPEXCHANGE_64(1)
+    BITONICSORT32_64() /**< Perform bitonic sort for 32-bit values using 64-bit warp exchange operations */ \
+    BITONICMERGE64_64() /**< Perform bitonic merge for 64-bit values using 64-bit warp exchange operations */ \
+    BITONICWARPEXCHANGE_64(16) /**< Perform bitonic warp exchange with mask 16 */ \
+    BITONICWARPEXCHANGE_64(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_64(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_64(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_64(1) /**< Perform bitonic warp exchange with mask 1 */
     
+/**
+ * @brief Perform bitonic warp exchange for 128-bit values based on a given mask.
+ *
+ * @param mask The mask used for bitonic warp exchange.
+ */
 #define BITONICWARPEXCHANGE_128(mask) \
     for (int i = 0; i < 4; i++) { \
         int idx1 = i; \
@@ -89,25 +108,31 @@
         v[idx1 + 4] = flag ? value1 : value2; \
     }
 
+/**
+ * @brief Perform bitonic sort for 32-bit values using 128-bit warp exchange operations.
+ */
 #define BITONICSORT32_128() \
-    BITONICWARPEXCHANGE_128(1) \
-    BITONICWARPEXCHANGE_128(3) \
-    BITONICWARPEXCHANGE_128(1) \
-    BITONICWARPEXCHANGE_128(7) \
-    BITONICWARPEXCHANGE_128(2) \
-    BITONICWARPEXCHANGE_128(1) \
-    BITONICWARPEXCHANGE_128(15) \
-    BITONICWARPEXCHANGE_128(4) \
-    BITONICWARPEXCHANGE_128(2) \
-    BITONICWARPEXCHANGE_128(1) \
-    BITONICWARPEXCHANGE_128(31) \
-    BITONICWARPEXCHANGE_128(8) \
-    BITONICWARPEXCHANGE_128(4) \
-    BITONICWARPEXCHANGE_128(2) \
-    BITONICWARPEXCHANGE_128(1) 
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_128(3) /**< Perform bitonic warp exchange with mask 3 */ \
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_128(7) /**< Perform bitonic warp exchange with mask 7 */ \
+    BITONICWARPEXCHANGE_128(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_128(15) /**< Perform bitonic warp exchange with mask 15 */ \
+    BITONICWARPEXCHANGE_128(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_128(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_128(31) /**< Perform bitonic warp exchange with mask 31 */ \
+    BITONICWARPEXCHANGE_128(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_128(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_128(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */
 
 
 
+/**
+ * @brief Perform bitonic merge for 64-bit values using 128-bit warp exchange operations.
+ */
 #define BITONICMERGE64_128() \
     for (int i = 0; i < 2; i++) { \
         int idx1 = i; \
@@ -126,15 +151,21 @@
         v[idx2] = SHFL(value1, otgx); \
     }
 
+/**
+ * @brief Perform bitonic sort for 64-bit values using 128-bit warp exchange and merge operations.
+ */
 #define BITONICSORT64_128() \
-    BITONICSORT32_128() \
-    BITONICMERGE64_128() \
-    BITONICWARPEXCHANGE_128(16) \
-    BITONICWARPEXCHANGE_128(8) \
-    BITONICWARPEXCHANGE_128(4) \
-    BITONICWARPEXCHANGE_128(2) \
-    BITONICWARPEXCHANGE_128(1)
+    BITONICSORT32_128() /**< Perform bitonic sort for 32-bit values using 128-bit warp exchange operations */ \
+    BITONICMERGE64_128() /**< Perform bitonic merge for 64-bit values using 128-bit warp exchange operations */ \
+    BITONICWARPEXCHANGE_128(16) /**< Perform bitonic warp exchange with mask 16 */ \
+    BITONICWARPEXCHANGE_128(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_128(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_128(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */
 
+/**
+ * @brief Perform bitonic merge for 128-bit values using 128-bit warp exchange operations.
+ */
 #define BITONICMERGE128_128() \
     for (int i = 0; i < 2; i++) { \
         int idx1 = i; \
@@ -153,7 +184,9 @@
         v[idx2] = SHFL(value1, otgx); \
     }
 
-
+/**
+ * @brief Perform bitonic exchange for 32-bit values using 128-bit warp exchange operations.
+ */
 #define BITONICEXCHANGE32_128() \
     for (int i = 0; i < 2; i++) { \
         int idx1 = i; \
@@ -170,6 +203,9 @@
     }
 
 
+/**
+ * @brief Perform bitonic exchange for 64-bit values using 128-bit warp exchange operations.
+ */
 #define BITONICEXCHANGE64_128() \
     for (int i = 0; i < 2; i++) { \
         int idx1 = i; \
@@ -185,20 +221,24 @@
         } \
     }
 
+/**
+ * @brief Perform bitonic sort for 128-bit values using 128-bit warp exchange and merge operations.
+ */
 #define BITONICSORT128_128() \
-    BITONICSORT64_128() \
-    BITONICMERGE128_128() \
-    BITONICEXCHANGE32_128() \
-    BITONICWARPEXCHANGE_128(16) \
-    BITONICWARPEXCHANGE_128(8) \
-    BITONICWARPEXCHANGE_128(4) \
-    BITONICWARPEXCHANGE_128(2) \
-    BITONICWARPEXCHANGE_128(1)
+    BITONICSORT64_128() /**< Perform bitonic sort for 64-bit values using 128-bit warp exchange and merge operations */ \
+    BITONICMERGE128_128() /**< Perform bitonic merge for 128-bit values using 128-bit warp exchange operations */ \
+    BITONICEXCHANGE32_128() /**< Perform bitonic exchange for 32-bit values using 128-bit warp exchange operations */ \
+    BITONICWARPEXCHANGE_128(16) /**< Perform bitonic warp exchange with mask 16 */ \
+    BITONICWARPEXCHANGE_128(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_128(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_128(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_128(1) /**< Perform bitonic warp exchange with mask 1 */
 
-
-
-
-
+/**
+ * @brief Perform bitonic warp exchange for 256-bit values based on a given mask.
+ *
+ * @param mask The mask used for bitonic warp exchange.
+ */
 #define BITONICWARPEXCHANGE_256(mask) \
     for (int i = 0; i < 8; i++) { \
         int idx1 = i; \
@@ -216,23 +256,29 @@
         v[idx1] = flag ? value1 : value2; \
     }
 
+/**
+ * @brief Perform bitonic sort for 32-bit values using 256-bit warp exchange operations.
+ */
 #define BITONICSORT32_256() \
-    BITONICWARPEXCHANGE_256(1) \
-    BITONICWARPEXCHANGE_256(3) \
-    BITONICWARPEXCHANGE_256(1) \
-    BITONICWARPEXCHANGE_256(7) \
-    BITONICWARPEXCHANGE_256(2) \
-    BITONICWARPEXCHANGE_256(1) \
-    BITONICWARPEXCHANGE_256(15) \
-    BITONICWARPEXCHANGE_256(4) \
-    BITONICWARPEXCHANGE_256(2) \
-    BITONICWARPEXCHANGE_256(1) \
-    BITONICWARPEXCHANGE_256(31) \
-    BITONICWARPEXCHANGE_256(8) \
-    BITONICWARPEXCHANGE_256(4) \
-    BITONICWARPEXCHANGE_256(2) \
-    BITONICWARPEXCHANGE_256(1) 
+    BITONICWARPEXCHANGE_256(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_256(3) /**< Perform bitonic warp exchange with mask 3 */ \
+    BITONICWARPEXCHANGE_256(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_256(7) /**< Perform bitonic warp exchange with mask 7 */ \
+    BITONICWARPEXCHANGE_256(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_256(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_256(15) /**< Perform bitonic warp exchange with mask 15 */ \
+    BITONICWARPEXCHANGE_256(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_256(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_256(1) /**< Perform bitonic warp exchange with mask 1 */ \
+    BITONICWARPEXCHANGE_256(31) /**< Perform bitonic warp exchange with mask 31 */ \
+    BITONICWARPEXCHANGE_256(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_256(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_256(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_256(1) /**< Perform bitonic warp exchange with mask 1 */ 
 
+/**
+ * @brief Perform bitonic merge for 64-bit values using 256-bit warp exchange operations.
+ */
 #define BITONICMERGE64_256() \
     for (int i = 0; i < 4; i++) { \
         int idx1 = i; \
@@ -255,15 +301,18 @@
         k[idx2] = SHFL(key1, otgx); \
         v[idx2] = SHFL(value1, otgx); \
     }
-    
+
+/**
+ * @brief Perform bitonic sort for 64-bit values using 256-bit warp exchange and merge operations.
+ */
 #define BITONICSORT64_256() \
-    BITONICSORT32_256() \
-    BITONICMERGE64_256() \
-    BITONICWARPEXCHANGE_256(16) \
-    BITONICWARPEXCHANGE_256(8) \
-    BITONICWARPEXCHANGE_256(4) \
-    BITONICWARPEXCHANGE_256(2) \
-    BITONICWARPEXCHANGE_256(1)
+    BITONICSORT32_256() /**< Perform bitonic sort for 32-bit values using 256-bit warp exchange operations */ \
+    BITONICMERGE64_256() /**< Perform bitonic merge for 64-bit values using 256-bit warp exchange operations */ \
+    BITONICWARPEXCHANGE_256(16) /**< Perform bitonic warp exchange with mask 16 */ \
+    BITONICWARPEXCHANGE_256(8) /**< Perform bitonic warp exchange with mask 8 */ \
+    BITONICWARPEXCHANGE_256(4) /**< Perform bitonic warp exchange with mask 4 */ \
+    BITONICWARPEXCHANGE_256(2) /**< Perform bitonic warp exchange with mask 2 */ \
+    BITONICWARPEXCHANGE_256(1) /**< Perform bitonic warp exchange with mask 1 */
 
 #define BITONICMERGE128_256() \
     for (int i = 0; i < 4; i++) { \
