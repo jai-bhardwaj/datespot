112e1231b45e4350f7d4f67b9c29b25c6a347b82
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-21 11:27:55 +0200

    Update transformer.cpp

--- src/engine/transformer.cpp
+++ src/engine/transformer.cpp
@@ -1,52 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 constexpr int kNumLayers = 6;
 constexpr int kNumHeads = 8;
 constexpr int kHiddenSize = 512;
 constexpr int kFeedForwardSize = 2048;
 
-class TransformerEncoderLayer {
-public:
-    std::vector<float> operator()(const std::vector<float>& input) {
-        std::vector<float> self_attention_output = selfAttention(input);
-        std::vector<float> add_norm_output = addAndNormalize(input, self_attention_output);
-        std::vector<float> feed_forward_output = feedForward(add_norm_output);
-        std::vector<float> encoder_output = addAndNormalize(add_norm_output, feed_forward_output);
-        return encoder_output;
-    }
-
-private:
-    std::vector<float> selfAttention(const std::vector<float>& input) {
-        std::vector<float> attention_output;
-        return attention_output;
-    }
-
-    std::vector<float> addAndNormalize(const std::vector<float>& input, const std::vector<float>& output) {
-        std::vector<float> normalized_output;
-        return normalized_output;
-    }
-
-    std::vector<float> feedForward(const std::vector<float>& input) {
-        std::vector<float> ff_output;
-        return ff_output;
-    }
-};
-
 class TransformerModel {
-public:
-    TransformerModel() {
-        for (int i = 0; i < kNumLayers; i++) {
-            transformerEncoderLayers_.push_back(TransformerEncoderLayer());
+private:
+    class TransformerEncoderLayer {
+    public:
+        std::vector<float> operator()(const std::vector<float>& input) {
+            std::vector<float> self_attention_output = selfAttention(input);
+            std::vector<float> add_norm_output = addAndNormalize(input, self_attention_output);
+            std::vector<float> feed_forward_output = feedForward(add_norm_output);
+            std::vector<float> encoder_output = addAndNormalize(add_norm_output, feed_forward_output);
+            return encoder_output;
         }
-    }
+
+    private:
+        std::vector<float> selfAttention(const std::vector<float>& input) {
+            std::vector<float> attention_output;
+            const int input_size = input.size();
+
+            std::vector<float> attention_scores(input_size);
+            for (int i = 0; i < input_size; i++) {
+                for (int j = 0; j < input_size; j++) {
+                }
+            }
+            
+            // Apply softmax to the attention scores
+            std::vector<float> attention_weights(input_size);
+            float sum = 0.0f;
+            for (int i = 0; i < input_size; i++) {
+                attention_weights[i] = std::exp(attention_scores[i]);
+                sum += attention_weights[i];
+            }
+            for (int i = 0; i < input_size; i++) {
+                attention_weights[i] /= sum;
+            }
+            
+            // Calculate attention output using the attention weights
+            attention_output.reserve(input_size);
+            for (int i = 0; i < input_size; i++) {
+                float weighted_sum = 0.0f;
+                for (int j = 0; j < input_size; j++) {
+                    weighted_sum += attention_weights[j] * input[j];
+                }
+                attention_output.push_back(weighted_sum);
+            }
+            
+            return attention_output;
+        }
+
+        std::vector<float> addAndNormalize(const std::vector<float>& input, const std::vector<float>& output) {
+            std::vector<float> normalized_output;
+            // Implement add and normalize logic
+            return normalized_output;
+        }
+
+        std::vector<float> feedForward(const std::vector<float>& input) {
+            std::vector<float> ff_output;
+            // Implement feed-forward logic
+            return ff_output;
+        }
+    };
+
+    std::vector<TransformerEncoderLayer> transformerEncoderLayers_;
+
+public:
+    TransformerModel() : transformerEncoderLayers_(kNumLayers) {}
 
     std::vector<float> operator()(const std::vector<float>& input) {
         std::vector<float> encoder_output = input;
 
-        for (int i = 0; i < kNumLayers; i++) {
-            encoder_output = transformerEncoderLayers_[i](encoder_output);
+        for (auto& layer : transformerEncoderLayers_) {
+            encoder_output = layer(encoder_output);
         }
 
         return encoder_output;
@@ -68,7 +99,7 @@ public:
                 result.push_back(vec1[i] + vec2[i]);
             }
         } else {
-            std::cout << "Error: Vector sizes do not match!" << std::endl;
+            throw std::runtime_error("Error: Vector sizes do not match!");
         }
 
         return result;
@@ -80,18 +111,14 @@ int main() {
     TransformerModel transformerModel;
 
     std::vector<float> output = transformerModel(input);
-
     transformerModel.printOutput(output);
 
     std::vector<float> input2 = {5.0, 6.0, 7.0, 8.0};
     std::vector<float> output2 = transformerModel(input2);
-
     transformerModel.printOutput(output2);
 
     std::vector<float> sum = transformerModel.addVectors(output, output2);
-
     transformerModel.printOutput(sum);
 
     return 0;
 }
-
