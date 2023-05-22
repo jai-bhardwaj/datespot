c06be02d6641387e487324b5f1cd1ae37cbc7eee
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-22 23:07:18 +0200

    Update cdl.h

--- src/utils/cdl.h
+++ src/utils/cdl.h
@@ -9,23 +9,23 @@
  */
 struct CDL
 {
-    std::string     _networkFileName;       /**< The file name of the network. */
-    int             _randomSeed;            /**< The random seed value. */
-    Mode            _mode;                  /**< The mode of operation. */
-    std::string     _dataFileName;          /**< The file name of the data. */
+    std::string _networkFileName;       /**< The file name of the network. */
+    int _randomSeed;                    /**< The random seed value. */
+    Mode _mode;                         /**< The mode of operation. */
+    std::string _dataFileName;          /**< The file name of the data. */
 
-    int             _epochs;                /**< The number of epochs. */
-    int             _batch;                 /**< The batch size. */
-    float           _alpha;                 /**< The learning rate. */
-    float           _lambda;                /**< The regularization parameter. */
-    float           _mu;                    /**< The momentum parameter. */
-    int             _alphaInterval;         /**< The interval for changing the learning rate. */
-    float           _alphaMultiplier;       /**< The multiplier for changing the learning rate. */
-    TrainingMode    _optimizer;             /**< The optimization algorithm. */
-    std::string     _checkpointFileName;    /**< The file name for checkpointing. */
-    int             _checkpointInterval;    /**< The interval for checkpointing. */
-    bool            _shuffleIndexes;        /**< Flag indicating whether to shuffle indexes. */
-    std::string     _resultsFileName;       /**< The file name for storing results. */
+    int _epochs;                        /**< The number of epochs. */
+    int _batch;                         /**< The batch size. */
+    float _alpha;                       /**< The learning rate. */
+    float _lambda;                      /**< The regularization parameter. */
+    float _mu;                          /**< The momentum parameter. */
+    int _alphaInterval;                 /**< The interval for changing the learning rate. */
+    float _alphaMultiplier;             /**< The multiplier for changing the learning rate. */
+    TrainingMode _optimizer;            /**< The optimization algorithm. */
+    std::string _checkpointFileName;    /**< The file name for checkpointing. */
+    int _checkpointInterval;            /**< The interval for checkpointing. */
+    bool _shuffleIndexes;               /**< Flag indicating whether to shuffle indexes. */
+    std::string _resultsFileName;       /**< The file name for storing results. */
 
     /**
      * @brief Default constructor for CDL.
@@ -39,3 +39,12 @@ struct CDL
      */
     int Load_JSON(const std::string& fname);
 };
+
+/**
+ * @brief Function to load JSON configuration from file.
+ * @param fname The name of the JSON file to load.
+ * @return The status of the load operation.
+ */
+int CDL::Load_JSON(const std::string& fname)
+{
+}
