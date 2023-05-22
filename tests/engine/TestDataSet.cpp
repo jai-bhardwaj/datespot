2b5a2d298c64d6afb71295432ed8b28507c92786
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-22 22:05:12 +0200

    Update TestDataSet.cpp

--- tests/engine/TestDataSet.cpp
+++ tests/engine/TestDataSet.cpp
@@ -2,6 +2,11 @@
 #include "GpuTypes.h"
 #include "Types.h"
 #include "Layer.h"
+#include <span>
+
+/**
+ * @brief Fixture for testing DataSet class.
+ */
 
 class TestDataSetFixture : public ::testing::Test {
 protected:
@@ -163,10 +168,10 @@ TEST_F(TestDataSetFixture, testLoadSparseData) {
 
     DataSetDimensions dim = dataset.GetDimensions();
     size_t sparseDataSize = static_cast<size_t>(((double) dim._height * dim._width * dim._length) * examples * sparseDensity);
-    uint64_t sparseStart[examples];
-    uint64_t sparseEnd[examples];
-    Float sparseData[sparseDataSize];
-    uint32_t sparseIndex[sparseDataSize];
+    std::span<uint64_t> sparseStart(new uint64_t[examples]);
+    std::span<uint64_t> sparseEnd(new uint64_t[examples]);
+    std::span<Float> sparseData(new Float[sparseDataSize]);
+    std::span<uint32_t> sparseIndex(new uint32_t[sparseDataSize]);
 
     size_t sparseExampleSize = (sparseDataSize + examples - 1) / examples;
 
@@ -177,7 +182,7 @@ TEST_F(TestDataSetFixture, testLoadSparseData) {
         sparseEnd[i] = sparseStart[i] + sparseExampleSize;
     }
 
-    for (uint32_t i = 0; i < sparseDataSize; ++i) {
+    for (size_t i = 0; i < sparseDataSize; ++i) {
         sparseData[i] = static_cast<Float>(i + 1);
     }
 
@@ -185,7 +190,7 @@ TEST_F(TestDataSetFixture, testLoadSparseData) {
         sparseIndex[i] = static_cast<uint32_t>(i);
     }
 
-    dataset.LoadSparseData(sparseStart, sparseEnd, sparseData, sparseIndex);
+    dataset.LoadSparseData(sparseStart.data(), sparseEnd.data(), sparseData.data(), sparseIndex.data());
 
     EXPECT_EQ(sparseEnd[0], dataset.GetSparseDataPoints(0));
     for (uint32_t i = 0; i < sparseEnd[0]; ++i) {
@@ -199,13 +204,13 @@ TEST_F(TestDataSetFixture, testLoadSparseData_Overflow) {
 
     DataSetDimensions dim = dataset.GetDimensions();
     size_t sparseDataSize = static_cast<size_t>(((double) dim._height * dim._width * dim._length) * examples * sparseDensity);
-    uint64_t sparseStart[examples];
+    std::span<uint64_t> sparseStart(new uint64_t[examples]);
     sparseStart[0] = 0;
-    uint64_t sparseEnd[examples];
+    std::span<uint64_t> sparseEnd(new uint64_t[examples]);
     Float sparseData[1];
     uint32_t sparseIndex[1];
     sparseEnd[examples - 1] = sparseDataSize + 1;
-    EXPECT_THROW(dataset.LoadSparseData(sparseStart, sparseEnd, sparseData, sparseIndex), std::length_error);
+    EXPECT_THROW(dataset.LoadSparseData(sparseStart.data(), sparseEnd.data(), sparseData, sparseIndex), std::length_error);
 }
 
 TEST_F(TestDataSetFixture, testLoadSparseData_SparseStartNotZeroIndexed) {
@@ -213,32 +218,32 @@ TEST_F(TestDataSetFixture, testLoadSparseData_SparseStartNotZeroIndexed) {
 
     DataSetDimensions dim = dataset.GetDimensions();
     size_t sparseDataSize = static_cast<size_t>(((double) dim._height * dim._width * dim._length) * examples * sparseDensity);
-    uint64_t sparseStart[examples];
+    std::span<uint64_t> sparseStart(new uint64_t[examples]);
     sparseStart[0] = 1;
-    uint64_t sparseEnd[examples];
+    std::span<uint64_t> sparseEnd(new uint64_t[examples]);
     Float sparseData[1];
     uint32_t sparseIndex[1];
     sparseEnd[examples - 1] = sparseDataSize + 1;
-    EXPECT_THROW(dataset.LoadSparseData(sparseStart, sparseEnd, sparseData, sparseIndex), std::runtime_error);
+    EXPECT_THROW(dataset.LoadSparseData(sparseStart.data(), sparseEnd.data(), sparseData, sparseIndex), std::runtime_error);
 }
 
 TEST_F(TestDataSetFixture, testLoadSparseData_OnDenseDataset) {
     DataSet<Float> dataset(examples, datasetDim);
-    uint64_t sparseStart[examples];
-    uint64_t sparseEnd[examples];
+    std::span<uint64_t> sparseStart(new uint64_t[examples]);
+    std::span<uint64_t> sparseEnd(new uint64_t[examples]);
     Float sparseData[1];
     uint32_t sparseIndex[1];
-    EXPECT_THROW(dataset.LoadSparseData(sparseStart, sparseEnd, sparseData, sparseIndex), std::runtime_error);
+    EXPECT_THROW(dataset.LoadSparseData(sparseStart.data(), sparseEnd.data(), sparseData, sparseIndex), std::runtime_error);
 }
 
 TEST_F(TestDataSetFixture, testLoadIndexedData) {
     DataSet<Float> dataset(examples, uniqueExamples, datasetDim);
-    uint32_t indexedData[uniqueExamples];
+    std::span<uint32_t> indexedData(new uint32_t[uniqueExamples]);
     for (uint32_t i = 0; i < uniqueExamples; ++i) {
         indexedData[i] = i;
     }
 
-    dataset.LoadIndexedData(indexedData);
+    dataset.LoadIndexedData(indexedData.data());
 
     for (uint32_t i = 0; i < uniqueExamples; ++i) {
         EXPECT_EQ(i, dataset._vIndex[i]);
@@ -247,19 +252,19 @@ TEST_F(TestDataSetFixture, testLoadIndexedData) {
 
 TEST_F(TestDataSetFixture, testLoadIndexedData_OotIndexedDataset) {
     DataSet<Float> dataset(examples, datasetDim);
-    uint32_t indexedData[examples];
-    EXPECT_THROW(dataset.LoadIndexedData(indexedData), std::runtime_error);
+    std::span<uint32_t> indexedData(new uint32_t[examples]);
+    EXPECT_THROW(dataset.LoadIndexedData(indexedData.data()), std::runtime_error);
 }
 
 TEST_F(TestDataSetFixture, testLoadDataWeights) {
     DataSet<uint32_t> dataset(examples, sparseDensity, datasetDim, true);
 
-    Float dataWeights[examples];
+    std::span<Float> dataWeights(new Float[examples]);
     for (uint32_t i = 0; i < examples; ++i) {
         dataWeights[i] = static_cast<Float>(i);
     }
 
-    dataset.LoadDataWeight(dataWeights);
+    dataset.LoadDataWeight(dataWeights.data());
 
     for (uint32_t i = 0; i < examples; ++i) {
         EXPECT_EQ(static_cast<Float>(i), dataset._vDataWeight[i]);
@@ -269,8 +274,8 @@ TEST_F(TestDataSetFixture, testLoadDataWeights) {
 TEST_F(TestDataSetFixture, testLoadDataWeights_OotWeightedDataset) {
     DataSet<uint32_t> dataset(examples, sparseDensity, datasetDim, false);
 
-    Float dataWeights[examples];
-    EXPECT_THROW(dataset.LoadDataWeight(dataWeights), std::runtime_error);
+    std::span<Float> dataWeights(new Float[examples]);
+    EXPECT_THROW(dataset.LoadDataWeight(dataWeights.data()), std::runtime_error);
 }
 
 TEST_F(TestDataSetFixture, testDataSetTypes) {
@@ -278,15 +283,13 @@ TEST_F(TestDataSetFixture, testDataSetTypes) {
     DataSet<double> doubleDataset(examples, datasetDim);
     DataSet<unsigned char> unsignedCharDataset(examples, datasetDim);
     DataSet<char> charDataset(examples, datasetDim);
-    DataSet<uint32_t> unsignedIntDataset(examples, datasetDim);
-    DataSet<uint64_t> unsignedLongDataset(examples, datasetDim);
-    DataSet<int32_t> intDataset(examples, datasetDim);
-    DataSet<int64_t> longDataset(examples, datasetDim);
-
+    DataSet<std::make_unsigned<uint32_t>::type> unsignedIntDataset(examples, datasetDim);
+    DataSet<std::make_unsigned<uint64_t>::type> unsignedLongDataset(examples, datasetDim);
+    DataSet<std::make_signed<int32_t>::type> intDataset(examples, datasetDim);
+    DataSet<std::make_signed<int64_t>::type> longDataset(examples, datasetDim);
 }
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
