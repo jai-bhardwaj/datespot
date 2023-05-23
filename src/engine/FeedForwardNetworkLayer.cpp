82f819c24928ec21d8f837706e3cc7ccf0323f0c
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-23 04:21:36 +0200

    Create FeedForwardNetworkLayer.cpp

---
+++ src/engine/FeedForwardNetworkLayer.cpp
@@ -0,0 +1,21 @@
+#include "FeedForwardNetworkLayer.h"
+
+FeedForwardNetworkLayer::FeedForwardNetworkLayer(const std::string& name) {
+    // Implementation of the constructor
+}
+
+void FeedForwardNetworkLayer::initialize() {
+    // Implementation of the initialize method
+}
+
+void FeedForwardNetworkLayer::forward() {
+    // Implementation of the forward method
+}
+
+std::vector<float> FeedForwardNetworkLayer::linearTransform(const std::vector<float>& input, const std::vector<std::vector<float>>& weights, const std::vector<float>& biases) {
+    // Implementation of the linear transformation
+}
+
+std::vector<float> FeedForwardNetworkLayer::relu(const std::vector<float>& input) {
+    // Implementation of the ReLU activation function
+}
