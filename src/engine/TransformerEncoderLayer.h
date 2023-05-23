e7a003f1b8be84176555656b014d0b770f629ecb
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-23 06:06:30 +0200

    Update TransformerEncoderLayer.h

--- src/engine/TransformerEncoderLayer.h
+++ src/engine/TransformerEncoderLayer.h
@@ -5,17 +5,23 @@
 #include "MultiHeadAttentionLayer.h"
 #include "FeedForwardNetworkLayer.h"
 #include <string>
+#include <vector>
 
 class TransformerEncoderLayer : public Layer {
 private:
     MultiHeadAttentionLayer multiHeadAttentionLayer_;
     FeedForwardNetworkLayer feedForwardNetworkLayer_;
+    std::vector<float> input_;
+    std::vector<float> output_;
 
 public:
     TransformerEncoderLayer(const std::string& name);
 
     void initialize() override;
     void forward() override;
+
+private:
+    std::vector<float> layerNormalization_(const std::vector<float>& input);
 };
 
 #endif // TRANSFORMER_ENCODER_LAYER_H
