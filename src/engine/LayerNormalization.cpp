c6354528e9092b4fdd4f5ef1c9b80b4d38e1afd4
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-23 04:24:49 +0200

    Create LayerNormalization.cpp

---
+++ src/engine/LayerNormalization.cpp
@@ -0,0 +1,22 @@
+#include "LayerNormalization.h"
+#include <cmath>
+
+LayerNormalization::LayerNormalization(const std::string& name) {
+    // Implementation of the constructor
+}
+
+void LayerNormalization::initialize() {
+    // Implementation of the initialize method
+}
+
+void LayerNormalization::forward() {
+    // Implementation of the forward method
+}
+
+float LayerNormalization::mean() {
+    // Implementation of the mean calculation
+}
+
+float LayerNormalization::stdDev() {
+    // Implementation of the standard deviation calculation
+}
