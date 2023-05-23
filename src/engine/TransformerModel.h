db2250b47487ca82374dd19688199ad6cb17db7f
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-23 04:17:08 +0200

    Create TransformerModel.h

---
+++ src/engine/TransformerModel.h
@@ -0,0 +1,26 @@
+#ifndef TRANSFORMER_MODEL_H
+#define TRANSFORMER_MODEL_H
+
+#include "FeedForwardNetwork.h"
+#include "TransformerEncoderLayer.h"
+#include "PositionalEncoding.h"
+#include <vector>
+
+class TransformerModel : public FeedForwardNetwork {
+private:
+    std::vector<TransformerEncoderLayer> transformerEncoderLayers_;
+    PositionalEncoding positionalEncoding_;
+
+public:
+    TransformerModel(int numLayers, int numHeads, int hiddenSize, int feedForwardSize);
+
+    void initialize() override;
+    void forward() override;
+
+private:
+    std::vector<float> embeddingLayer_(const std::vector<float>& input);
+    std::vector<float> applyPositionalEncoding(const std::vector<float>& input);
+    std::vector<float> layerNormalization_(const std::vector<float>& input);
+};
+
+#endif // TRANSFORMER_MODEL_H
