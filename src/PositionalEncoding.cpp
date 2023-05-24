5cce6545cd8ca26c0239edbc24de4fa1428746da
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-24 20:39:40 +0200

    Create PositionalEncoding.cpp

---
+++ src/PositionalEncoding.cpp
@@ -0,0 +1,10 @@
+#include "PositionalEncoding.h"
+
+std::vector<std::vector<float>> PositionalEncoding::forward(const std::vector<std::vector<float>>& input) {
+    // TODO Implementation of forward pass
+}
+
+std::vector<std::vector<float>> PositionalEncoding::backward(const std::vector<std::vector<float>>& input, const std::vector<std::vector<float>>& grads) {
+    // TODO Implementation of backward pass
+}
+
