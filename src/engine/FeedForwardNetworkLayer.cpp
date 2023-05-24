3b83428f4543d710e2432442657c3b54cc690208
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-24 03:13:49 +0200

    Update FeedForwardNetworkLayer.cpp

--- src/engine/FeedForwardNetworkLayer.cpp
+++ src/engine/FeedForwardNetworkLayer.cpp
@@ -3,69 +3,62 @@
 #include <string>
 #include <chrono>
 #include <iostream>
+#include <vector>
 
-#include <Eigen/Dense>
 
 class FeedForwardNetworkLayer {
 private:
     std::string name;
-    Eigen::MatrixXf weights;
-    Eigen::VectorXf biases;
-    Eigen::VectorXf output;
+    std::vector<std::vector<float>> weights;
+    std::vector<float> biases;
+    std::vector<float> output;
     std::mt19937 randomEngine;
 
 public:
-    FeedForwardNetworkLayer(const std::string& name);
-    void initialize();
-    void forward(const Eigen::VectorXf& input);
-    void printOutput() const;
+    FeedForwardNetworkLayer(const std::string& name) : name(name), randomEngine(std::random_device{}()) {
+    }
+
+    void initialize() {
+        initializeRandomMatrix(0.0f, 0.1f);
+        initializeRandomVector(0.0f, 0.1f);
+    }
+
+    void forward(const std::vector<float>& input) {
+        std::transform(input.begin(), input.end(), weights.begin(), output.begin(), std::plus<float>());
+        std::transform(output.begin(), output.end(), biases.begin(), output.begin(), std::plus<float>());
+        std::transform(output.begin(), output.end(), output.begin(), [](float x) { return std::max(0.0f, x); });
+    }
+
+    void printOutput() const {
+        std::cout << "Layer: " << name << "\n";
+        std::cout << "Output:\n";
+        std::copy(output.begin(), output.end(), std::ostream_iterator<float>(std::cout, " "));
+        std::cout << "\n";
+    }
 
 private:
-    Eigen::VectorXf linearTransform(const Eigen::VectorXf& input) const;
-    Eigen::VectorXf relu(const Eigen::VectorXf& input) const;
-    void initializeRandomMatrix(float mean, float stddev);
-    void initializeRandomVector(float mean, float stddev);
+    void initializeRandomMatrix(float mean, float stddev) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> distribution(mean, stddev);
+
+        weights.resize(output.size());
+        for (size_t i = 0; i < weights.size(); ++i) {
+            weights[i].resize(weights.size());
+            for (size_t j = 0; j < weights[i].size(); ++j) {
+                weights[i][j] = distribution(gen);
+            }
+        }
+    }
+
+    void initializeRandomVector(float mean, float stddev) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> distribution(mean, stddev);
+
+        biases.resize(output.size());
+        for (size_t i = 0; i < biases.size(); ++i) {
+            biases[i] = distribution(gen);
+        }
+    }
 };
-
-FeedForwardNetworkLayer::FeedForwardNetworkLayer(const std::string& name) : name(name), randomEngine(std::random_device{}()) {
-}
-
-void FeedForwardNetworkLayer::initialize() {
-    initializeRandomMatrix(0.0f, 0.1f);
-    initializeRandomVector(0.0f, 0.1f);
-}
-
-void FeedForwardNetworkLayer::forward(const Eigen::VectorXf& input) {
-    Eigen::VectorXf transformed = linearTransform(input);
-    Eigen::VectorXf activated = relu(transformed);
-    output = activated;
-}
-
-Eigen::VectorXf FeedForwardNetworkLayer::linearTransform(const Eigen::VectorXf& input) const {
-    return weights * input + biases;
-}
-
-Eigen::VectorXf FeedForwardNetworkLayer::relu(const Eigen::VectorXf& input) const {
-    return input.cwiseMax(0.0f);
-}
-
-void FeedForwardNetworkLayer::initializeRandomMatrix(float mean, float stddev) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<float> distribution(mean, stddev);
-
-    weights = Eigen::MatrixXf::NullaryExpr(weights.rows(), weights.cols(), [&]() { return distribution(gen); });
-}
-
-void FeedForwardNetworkLayer::initializeRandomVector(float mean, float stddev) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<float> distribution(mean, stddev);
-
-    biases = Eigen::VectorXf::NullaryExpr(biases.size(), [&]() { return distribution(gen); });
-}
-
-void FeedForwardNetworkLayer::printOutput() const {
-    std::cout << "Layer: " << name << "\n";
-    std::cout << "Output:\n" << output << "\n";
-}
