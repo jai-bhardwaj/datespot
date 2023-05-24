69df55f68c2de6765618783f3650aeedc4dc99e6
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-24 05:14:54 +0200

    Create LayerNorm.cpp

---
+++ src/engine/LayerNorm.cpp
@@ -0,0 +1,9 @@
+#include "LayerNorm.h"
+
+std::vector<std::vector<float>> LayerNorm::forward(const std::vector<std::vector<float>>& input) {
+    // TODO Implementation of forward pass
+}
+
+std::vector<std::vector<float>> LayerNorm::backward(const std::vector<std::vector<float>>& input, const std::vector<std::vector<float>>& grads) {
+    // TODO Implementation of backward pass
+}
