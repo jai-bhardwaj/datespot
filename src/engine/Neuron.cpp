0702b81d344d6d35f2cb9f478bf3d058bd81cc65
dimske-sys <dempsey.de.clerck@hotmail.com>
2023-05-24 02:44:06 +0200

    Update Neuron.cpp

--- src/engine/Neuron.cpp
+++ src/engine/Neuron.cpp
@@ -1,29 +1,55 @@
 #include "Neuron.h"
+#include <stdexcept>
+#include <cmath>
 
-Neuron::Neuron(const std::string& name) {
-    // Implementation of the constructor
+Neuron::Neuron(const std::string& name)
+  : name(name), weight(0.0f), input(0.0f), output(0.0f) {
 }
 
-void Neuron::initialize() {
-    // Implementation of the initialize method
+void Neuron::setWeight(float weight) {
+  if (weight >= 0) {
+    this->weight = weight;
+  } else {
+    throw std::invalid_argument("Weight must be a non-negative value.");
+  }
 }
 
-LinearNeuron::LinearNeuron(const std::string& name) : Neuron(name) {
-    // Implementation of the constructor
+float Neuron::getWeight() const {
+  return weight;
+}
+
+void Neuron::setInput(float input) {
+  this->input = input;
+}
+
+float Neuron::getInput() const {
+  return input;
+}
+
+float Neuron::getOutput() const {
+  return output;
 }
 
 void LinearNeuron::forward() {
-    // Implementation of the forward method for LinearNeuron
+  output = getInput() * getWeight();
 }
 
-ActivationNeuron::ActivationNeuron(const std::string& name) : Neuron(name) {
-    // Implementation of the constructor
+void LinearNeuron::backward(float learningRate) {
+  float gradient = getInput();
+  float weightUpdate = learningRate * gradient;
+  setWeight(getWeight() - weightUpdate);
 }
 
 void ActivationNeuron::forward() {
-    // Implementation of the forward method for ActivationNeuron
+  output = activationFunc(getInput());
 }
 
-float ActivationNeuron::activate(float x) {
-    // Implementation of the activation function
+void ActivationNeuron::backward(float learningRate) {
+  float gradient = getInput() * derivative;
+  float weightUpdate = learningRate * gradient;
+  setWeight(getWeight() - weightUpdate);
+}
+
+float sigmoidActivationFunction(float x) {
+  return 1.0f / (1.0f + std::exp(-x));
 }
